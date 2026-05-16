66ea6be83cf376becd5f1be89940af69b067cab9
ospik14 <bliznakstap@gmail.com>
2026-05-16 21:13:36 +0300

    conected database and created table for posts

---
+++ src/db_models/tables_models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from core.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func, DateTime
+
+class Post(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_url: Mapped[str | None] = mapped_column(default=None) 
+    title: Mapped[str | None] = mapped_column(default=None)
+    text: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now()
+    )
